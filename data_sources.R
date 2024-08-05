0b4e1a615dd73eedc62ec4103bd9346128757704
nzgwynn <nzgwynn@gmail.com>
2024-08-05 16:49:49 -0400

    commit data sources

---
+++ data_sources.R
@@ -0,0 +1,5 @@
+## Flight tracker page: https://www.flightradar24.com/data/airports/pva
+
+## Actual flights in the air now: https://www.flightaware.com/live/
+
+## Satena flights: https://www.flightaware.com/live/fleet/NSE
